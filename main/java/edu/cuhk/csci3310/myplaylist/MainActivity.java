b4a86413b07e2f6452aa25d7124200ecb59f9e3d
Flos <70808554+F-los@users.noreply.github.com>
2023-04-24 14:30:57 +0800

    Add files via upload

---
+++ main/java/edu/cuhk/csci3310/myplaylist/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.cuhk.csci3310.myplaylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void popup
+}
